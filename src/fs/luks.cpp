df33b21b0d10306aedeb0498443cfaa0d175bfd6
Andrius Štikonas <andrius@stikonas.eu>
2016-04-26 13:46:02 +0100

    Optimize QString->QStringLiteral.

--- src/fs/luks.cpp
+++ src/fs/luks.cpp
@@ -314,7 +314,7 @@ bool luks::cryptClose(const QString& deviceNode)
     m_innerFs = nullptr;
 
     m_passphrase.clear();
-    setLabel({});
+    setLabel(QStringLiteral());
 
     m_isCryptOpen = (m_innerFs != nullptr);
 
@@ -519,7 +519,7 @@ QString luks::mapperName(const QString& deviceNode)
         if (rxDeviceName.indexIn(cmd.output()) > -1)
             return rxDeviceName.cap(1);
     }
-    return QString();
+    return QStringLiteral();
 }
 
 QString luks::getCipherName(const QString& deviceNode)
