d033dc982781e0741505178f338d930554a0e63c
Chantara Tith <tith.chantara@gmail.com>
2016-08-08 12:53:24 +0700

    Add deactivating VG and LV jobs.

---
+++ src/jobs/deactivatelogicalvolumejob.cpp
@@ -0,0 +1,63 @@
+/*************************************************************************
+ *  Copyright (C) 2016 by Chantara Tith <tith.chantara@gmail.com>        *
+ *                                                                       *
+ *  This program is free software; you can redistribute it and/or        *
+ *  modify it under the terms of the GNU General Public License as       *
+ *  published by the Free Software Foundation; either version 3 of       *
+ *  the License, or (at your option) any later version.                  *
+ *                                                                       *
+ *  This program is distributed in the hope that it will be useful,      *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of       *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
+ *  GNU General Public License for more details.                         *
+ *                                                                       *
+ *  You should have received a copy of the GNU General Public License    *
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.*
+ *************************************************************************/
+
+#include "jobs/deactivatelogicalvolumejob.h"
+
+#include "core/lvmdevice.h"
+#include "core/partition.h"
+#include "core/partitiontable.h"
+
+#include "util/report.h"
+
+#include <KLocalizedString>
+
+/** Creates a new DeactivateLogicalVolumeJob
+    @param vgname
+    @parem pvList
+*/
+DeactivateLogicalVolumeJob::DeactivateLogicalVolumeJob(VolumeManagerDevice& dev, QStringList lvPaths) :
+    Job(),
+    m_Device(dev),
+    m_LVList(lvPaths)
+{
+}
+
+bool DeactivateLogicalVolumeJob::run(Report& parent)
+{
+    bool rval = true;
+
+    Report* report = jobStarted(parent);
+
+    if (device().type() == Device::LVM_Device) {
+        foreach (Partition* part, device().partitionTable()->children()) {
+            if (!part->roles().has(PartitionRole::Unallocated)) {
+                if (!LvmDevice::deactivateLV(*report, dynamic_cast<LvmDevice&>(device()), *part)) {
+                    rval = false;
+                }
+            }
+        }
+    }
+
+    jobFinished(*report, rval);
+
+    return rval;
+}
+
+QString DeactivateLogicalVolumeJob::description() const
+{
+    return xi18nc("@info/plain", "Deactivate Logical Volumes: <filename>%1</filename>", device().prettyDeviceNodeList());
+}
---
+++ src/jobs/deactivatevolumegroupjob.cpp
@@ -0,0 +1,54 @@
+/*************************************************************************
+ *  Copyright (C) 2016 by Chantara Tith <tith.chantara@gmail.com>        *
+ *                                                                       *
+ *  This program is free software; you can redistribute it and/or        *
+ *  modify it under the terms of the GNU General Public License as       *
+ *  published by the Free Software Foundation; either version 3 of       *
+ *  the License, or (at your option) any later version.                  *
+ *                                                                       *
+ *  This program is distributed in the hope that it will be useful,      *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of       *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
+ *  GNU General Public License for more details.                         *
+ *                                                                       *
+ *  You should have received a copy of the GNU General Public License    *
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.*
+ *************************************************************************/
+
+#include "jobs/deactivatevolumegroupjob.h"
+
+#include "core/lvmdevice.h"
+
+#include "util/report.h"
+
+#include <KLocalizedString>
+
+/** Deactivate LVM Volume Group
+    @param vgname
+    @parem pvList
+*/
+DeactivateVolumeGroupJob::DeactivateVolumeGroupJob(VolumeManagerDevice& dev) :
+    Job(),
+    m_Device(dev)
+{
+}
+
+bool DeactivateVolumeGroupJob::run(Report& parent)
+{
+    bool rval = false;
+
+    Report* report = jobStarted(parent);
+
+    if (device().type() == Device::LVM_Device) {
+        rval = LvmDevice::deactivateVG(*report, dynamic_cast<LvmDevice&>(device()));
+    }
+
+    jobFinished(*report, rval);
+
+    return rval;
+}
+
+QString DeactivateVolumeGroupJob::description() const
+{
+    return xi18nc("@info/plain", "Deactivate Volume Group: <filename>%1</filename>", device().name());
+}
