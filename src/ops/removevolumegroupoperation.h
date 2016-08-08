9ed640084d8fe60f445ad002da32ceceb58dba56
Andrius Štikonas <andrius@stikonas.eu>
2016-08-08 17:32:40 +0100

    Fix broken include guard.

--- src/ops/removevolumegroupoperation.h
+++ src/ops/removevolumegroupoperation.h
@@ -15,7 +15,7 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.*
  *************************************************************************/
 
-#if !defined(REMOVEVOLUMEOPERATION_H)
+#if !defined(REMOVEVOLUMEGROUPOPERATION_H)
 
 #define REMOVEVOLUMEGROUPOPERATION_H
 
