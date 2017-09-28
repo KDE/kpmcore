8c3ab58d84358f3e0289ab9aacee232764b3ea9d
Andrius Štikonas <andrius@stikonas.eu>
2017-09-28 14:24:13 +0100

    Add a comment about FAT volume ID hack.

--- src/fs/fat32.cpp
+++ src/fs/fat32.cpp
@@ -56,6 +56,7 @@ bool fat32::updateUUID(Report& report, const QString& deviceNode) const
         t >>= 8;
     }
 
+    // HACK: replace this hack with fatlabel "-i" (dosfstools 4.2)
     ExternalCommand cmd(report, QStringLiteral("dd"), { QStringLiteral("of=") + deviceNode, QStringLiteral("bs=1"), QStringLiteral("count=4"), QStringLiteral("seek=67") });
 
     if (!cmd.start())
