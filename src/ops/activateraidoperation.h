2d8a800a6dfba8fb50d0c2ce45b7df0d692b7191
Andrius Štikonas <andrius@stikonas.eu>
2020-10-02 22:05:40 +0100

    REUSE: SPDX headers for new files in raid-support branch.

--- src/ops/activateraidoperation.h
+++ src/ops/activateraidoperation.h
@@ -1,22 +1,10 @@
-/*************************************************************************
- *  Copyright (C) 2018 by Caio Carvalho <caiojcarvalho@gmail.com>        *
- *                                                                       *
- *  This program is free software; you can redistribute it and/or        *
- *  modify it under the terms of the GNU General Public License as       *
- *  published by the Free Software Foundation; either version 3 of       *
- *  the License, or (at your option) any later version.                  *
- *                                                                       *
- *  This program is distributed in the hope that it will be useful,      *
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of       *
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
- *  GNU General Public License for more details.                         *
- *                                                                       *
- *  You should have received a copy of the GNU General Public License    *
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.*
- *************************************************************************/
+/*
+    SPDX-FileCopyrightText: 2018 Caio Jordão Carvalho <caiojcarvalho@gmail.com>
 
-#if !defined(KPMCORE_ACTIVATERAIDOPERATION_H)
+    SPDX-License-Identifier: GPL-3.0-or-later
+*/
 
+#ifndef KPMCORE_ACTIVATERAIDOPERATION_H
 #define KPMCORE_ACTIVATERAIDOPERATION_H
 
 #include "ops/operation.h"
